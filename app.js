7f8c1674a710efe964bbf7c6e8efed5421b597a4
Michael Conrad <michael@threepears.com>
2016-02-25 15:12:50 -0600

    Create SQL queries on two different routes, include Moment.js

--- app.js
+++ app.js
@@ -1,17 +1,73 @@
 "use strict";
 
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("./db/chinook.sqlite");
 
-console.log("# of invoices per country");
+app.get("/sales-per-year", (req, res) => {
 
-db.each(`
-  SELECT    COUNT(*) as InvoiceCount,
-            BillingCountry AS Country
-  FROM      Invoice
-  GROUP BY  BillingCountry
-  ORDER BY  InvoiceCount DESC`,
-    (err, res) => {
-      console.log(res);
-    }
-);
+  let having = "";
+
+  if (req.query.filter) {
+    having = "HAVING";
+
+    req.query.filter.year
+      .split(",")
+      .map(y => +y)
+      .forEach(y => {
+        having += ` year = "${y}" OR`;
+    });
+
+    having = having.substring(0, having.length - 3);
+  }
+
+  db.all(`
+    SELECT    COUNT(*) as Invoices,
+              SUM(Total) as Total,
+              STRFTIME("%Y", InvoiceDate) as year
+    FROM      Invoice
+    GROUP BY  year
+    ${having}`,
+      (err, data) => {
+        if (err) throw err;
+
+        const roundedData = data.map(function (obj) {
+          return {
+            invoices: obj.invoices,
+            year: +obj.year,
+            total: +obj.Total.toFixed(2)
+          }
+        });
+
+        res.send({
+          data: roundedData,
+          info: "# of invoices and sales per year"
+        });
+      })
+});
+
+app.get("/invoices-per-country", (req, res) => {
+  db.all(`
+    SELECT    COUNT(*) as InvoiceCount,
+              BillingCountry AS Country
+    FROM      Invoice
+    GROUP BY  BillingCountry
+    ORDER BY  InvoiceCount DESC`,
+      (err, data) => {
+        if (err) throw err;
+
+        res.send({
+          data: data,
+          info: "# of invoices per country"
+        });
+      }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+})
